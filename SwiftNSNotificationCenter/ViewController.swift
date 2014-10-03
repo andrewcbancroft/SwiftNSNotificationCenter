90d225357f23aa96209b1708204311d40ebe9a42
Andrew Bancroft <andrewcbancroft@gmail.com>
2014-10-02 22:08:18 -0500

    Initial Commit

---
+++ SwiftNSNotificationCenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftNSNotificationCenter
+//
+//  Created by Andrew Bancroft on 10/2/14.
+//  Copyright (c) 2014 Andrew Bancroft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
