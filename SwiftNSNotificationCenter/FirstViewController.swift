1fb441e59a5a222e1c45e8e6efced00ce7b663db
Andrew Bancroft <andrewcbancroft@gmail.com>
2014-10-08 13:48:06 -0500

    initial commit

---
+++ SwiftNSNotificationCenter/FirstViewController.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+
+let mySpecialNotificationKey = "com.andrewcbancroft.specialNotificationKey"
+
+class FirstViewController: UIViewController {
+	@IBOutlet weak var sentNotificationLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateNotificationSentLabel", name: mySpecialNotificationKey, object: nil)
+	}
+	
+	@IBAction func notify() {
+		NSNotificationCenter.defaultCenter().postNotificationName(mySpecialNotificationKey, object: self)
+	}
+	
+	func updateNotificationSentLabel() {
+		self.sentNotificationLabel.text = "Notification sent!"
+	}
+}
+
---
+++ SwiftNSNotificationCenter/SecondViewController.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+	@IBOutlet weak var notificationLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "actOnSpecialNotification", name: mySpecialNotificationKey, object: nil)
+	}
+	
+	func actOnSpecialNotification() {
+		self.notificationLabel.text = "I heard the notification!"
+	}
+}
